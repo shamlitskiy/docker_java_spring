5134083100b063451f456cbf6d1992abeff57ff9
Aleksey Shamlitskiy <ashamlitskiy@gmail.com>
2025-07-06 15:49:35 +0000

    Base CRUD of task manager has been added.

---
+++ simple-api/src/main/java/ru/example/simple_api/SimpleApiApplication.java
@@ -0,0 +1,14 @@
+package ru.example.simple_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiApplication.class, args);
+	}
+
+}
